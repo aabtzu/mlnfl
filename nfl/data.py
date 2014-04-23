d9a5511a463b79a2e35d75a883565cc4e9e3f32f
Amit Bhattacharyya <Amit.Bhattacharyya@annalect.com>
2014-04-23 11:44:12 -0400

    add other probabliity based guesses

--- nfl/data.py
+++ nfl/data.py
@@ -8,11 +8,14 @@ import lookup
 
 
 def getWeek(seasonStart,gameDateStr):
-    gameDate = dp.parse(gameDateStr).date()
-    week = int(np.ceil((gameDate - seasonStart).days / 7.0))+1
-    return week
+# determine week of season
+	gameDate = dp.parse(gameDateStr).date()
+	week = int(np.ceil((gameDate - seasonStart).days / 7.0))+1
+	return week
 
 def sameDivision(team1,team2,olookups):
+# determine if two teams are in the same division
+
     conf1 = olookups.lookupCSV('teams',team1,'league')
     conf2 = olookups.lookupCSV('teams',team2,'league')
     
@@ -28,6 +31,8 @@ def sameDivision(team1,team2,olookups):
     
 
 def readGames(dataRoot,season):
+	# read a csv file of game scores and spreads into a pandas data favoredHomeGame
+	#
 
 	dataFile = dataRoot + "lines/nfl%slines.csv" % season
 	dfAllGames = pandas.read_csv(dataFile)
@@ -36,9 +41,9 @@ def readGames(dataRoot,season):
 
 
 def seasonRecord(dfAllGames,olookups):
-
-# compile season stats by team
-# games played, games won, lost, record to date, home/away, division game, 
+	# compile season stats by team
+	# games played, games won, lost, record to date, home/away, division game, 
+	#
 
 	teams = dfAllGames.Visitor.unique()
 
@@ -76,6 +81,8 @@ def seasonRecord(dfAllGames,olookups):
 	return dfAllTeams
 
 def getSeasonStart(olookups,season):
+	# lookup start of season for a given year
+	#
 
 	seasonStr = olookups.lookupCSV('seasons',season,'start')
 	seasonStart = dp.parse(seasonStr).date()
@@ -83,6 +90,8 @@ def getSeasonStart(olookups,season):
 	return seasonStart
 
 def processGames(dfAllGames, dfAllTeams, olookups, season):
+	# apply season record and other stats to all games played
+	#
 
 	seasonStart = getSeasonStart(olookups,season)
 
@@ -158,6 +167,8 @@ def processGames(dfAllGames, dfAllTeams, olookups, season):
 	return dfAllGames
 
 def getTrainData(dfAllGames,maxTrainWeek=17):
+	# extract features and classifiers from data frame for sklearn routines
+	#
 
 	dfTrain = dfAllGames[dfAllGames.gameWeek <= maxTrainWeek]
 	y = dfTrain.favoredWin.tolist()
@@ -169,6 +180,9 @@ def getTrainData(dfAllGames,maxTrainWeek=17):
 	return X,y
 
 def runLogisticRegression(X,y):
+	# get logistic regression object for features/classifiers
+	#
+
 	logreg = linear_model.LogisticRegression(C=1e5)
 	logreg.fit(X,y)	
 
@@ -178,13 +192,16 @@ def runLogisticRegression(X,y):
 	return logreg
 
 def runML(dfAllGames,maxTrainWeek=17):
+	# run machine learning on training data
+	#
 
 	train_X,train_y = getTrainData(dfAllGames,maxTrainWeek)
 	logreg = runLogisticRegression(train_X,train_y)
 	return logreg
 
 def predictGames(dfAllGames,logreg):
-
+	# apply results of logistic regression to test data
+	#
 	dfPredict = dfAllGames
 	predict_X, predict_y = getTrainData(dfAllGames)
 
@@ -192,43 +209,54 @@ def predictGames(dfAllGames,logreg):
 	pp = logreg.predict_proba(predict_X)
 	dfxn = logreg.decision_function(predict_X)
 
-	dfPredict['predict'] = p
 	dfPredict['predict_proba'] = pp[:,1]
+	dfPredict['predict_proba_abs'] = abs(pp[:,1] - 0.5)
 	dfPredict['decision_fxn'] = dfxn
 
+	scoreDiff = dfPredict['Home Score'] - dfPredict['Visitor Score']
+	dfPredict['predictWin'] = 1 * ((scoreDiff * dfPredict['Line'] * (dfPredict['predict_proba'] - 0.5) )> 0)
 	return dfPredict
 
 def rankGames(dfPredict):
+	# deterime weekly rankings for games based on predict from logistic regression
+	#
 
 	weeks = dfPredict.gameWeek.unique()
-
 	dfAll = None
 
 	for ww in weeks:
-	    iw = dfPredict[dfPredict.gameWeek == ww].index
-	    dfWeek = dfPredict.loc[iw]
-	    nw = len(iw)
-	    dfLine = dfWeek.sort(['absLine','favoredHomeGame','predict_proba'])
+		iw = dfPredict[dfPredict.gameWeek == ww].index
+		dfWeek = dfPredict.loc[iw]
+		nw = len(iw)
+		dfLine = dfWeek.sort(['absLine','favoredHomeGame','predict_proba'])
 		# print ww,nw
 	    
 	    # determine guess 
-	    dfLine['lineGuess'] = dfLine['absLine'].rank('first') + (16 - nw)
-	    dfLine['probaGuess'] = dfLine['predict_proba'].rank('first') + (16 - nw)
+	    # several possibilities for predicting by probability
+	    # 1. always pick favored team, rank by probablity of win
+	    # 2. pick winner based on abs(probability - .5), rank by probability
+	    # 3. pick winner based on abs(probability - .5), rank by abs(probability - .5)
+		dfLine['lineGuess'] = dfLine['absLine'].rank('first') + (16 - nw)
+		dfLine['probaGuess'] = dfLine['predict_proba'].rank('first') + (16 - nw)
+		dfLine['probaAbsGuess'] = dfLine['predict_proba_abs'].rank('first') + (16 - nw)
 	    
 	    # determine score
-	    dfLine['lineScore'] = dfLine['lineGuess'] * dfLine['favoredWin']
-	    dfLine['probaScore'] = dfLine['probaGuess'] * dfLine['favoredWin']
-	    
-	    if ww == 1:
-	        dfAll = dfLine
-	    else:
-	        dfAll = dfAll.append(dfLine)	
+		dfLine['lineScore'] = dfLine['lineGuess'] * dfLine['favoredWin']
+
+		dfLine['probaScore1'] = dfLine['probaGuess'] * dfLine['favoredWin']  
+		dfLine['probaScore2'] = dfLine['probaGuess'] * dfLine['predictWin']  
+		dfLine['probaScore3'] = dfLine['probaAbsGuess'] * dfLine['predictWin']  
+
+		if ww == 1:
+			dfAll = dfLine
+		else:
+			dfAll = dfAll.append(dfLine)	
 
 
-	dispCols = ['gameWeek','Visitor','Home Team','Line','predict','predict_proba','Visitor Score','Home Score','favoredWin','lineGuess','probaGuess', 'lineScore','probaScore']
+	dispCols = ['gameWeek','Visitor','Home Team','Line','predict_proba','Visitor Score','Home Score','favoredWin','predictWin','lineGuess','probaGuess', 'lineScore','probaScore']
 	#print dfAll[dfAll.gameWeek==1][dispCols]
 
-	g = dfAll.groupby('gameWeek')['lineScore','probaScore'].sum()
+	g = dfAll.groupby('gameWeek')['lineScore','probaScore1','probaScore2','probaScore3'].sum()
 	print g
 	print g.sum()
 
