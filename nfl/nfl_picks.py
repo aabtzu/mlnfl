2bdd5d11c7edd04ee1c71ba6bafb2b6a51c6fa9d
littlea1 <alain.ledon@gmail.com>
2015-09-16 01:47:12 -0400

    Refactor most of the data frame variables Added  command line parameter to pass the week Added csv output for the three possible strategies

--- nfl/nfl_picks.py
+++ nfl/nfl_picks.py
@@ -8,21 +8,27 @@ WEEK_TO_PICK = 1
 import os
 import numpy as np
 import pandas as pd
+import sys
+import madden
+import logging
+import argparse
+
+from sklearn import linear_model
+from sklearn import svm
+from referencedata import ReferenceData
 
 pd.options.mode.chained_assignment = None
 pd.set_option('expand_frame_repr', False)
 
-import logging
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
 
-import madden
+parser = argparse.ArgumentParser()
+parser.add_argument('-w', '--week', default=WEEK_TO_PICK, dest='game_week', help='Pass the week number to make the picks')
+args = parser.parse_args()
 
-from sklearn import linear_model
-from sklearn import svm
-from sklearn import ensemble
-
-from referencedata import ReferenceData
+# predict one week of current season
+week_number = args.game_week
 
 # define the root directory for the nfl code in $MLNLF_ROOT
 codeDir = "".join([os.environ['MLNFL_ROOT'], os.path.sep])
@@ -56,13 +62,13 @@ logging.info("training seasons >> {0}".format(seasons))
 # get training data
 # 1 - read all the games
 path_to_lines = dataRoot + "lines/"
-dfAllGames = madden.readGamesAll(path_to_lines, seasons)
+df_all_historical_games = madden.readGamesAll(path_to_lines, seasons)
 # 2 - compile season record for all teams
-dfAllTeams = madden.seasonRecord(dfAllGames, reference_data)
+df_all_teams = madden.seasonRecord(df_all_historical_games, reference_data)
 # 3 - apply season records and compute other fields for all games
-dfAllGames = madden.processGames(dfAllGames, dfAllTeams, reference_data)
+df_all_historical_games = madden.processGames(df_all_historical_games, df_all_teams, reference_data)
 # 4 - remove extra year of data
-dfAllGames = dfAllGames[dfAllGames.season.isin(seasons)]
+df_all_historical_games = df_all_historical_games[df_all_historical_games.season.isin(seasons)]
 
 # use different test set
 season_test = np.array(testYear) # should be only one year
@@ -76,25 +82,13 @@ dfGamesTest = madden.processGames(dfGamesTest, dfTeamsTest, reference_data)
 # 4 - remove extra year of data
 dfGamesTest = dfGamesTest[dfGamesTest.season.isin(season_test)]
 
-# define independent variables for logistic regression
-features = ['favoredRecord','underdogRecord',  # current year records of both teams
-            'prevFavoredRecord','prevUnderdogRecord', # prev year records, helps early in season when only few games played
-            'gameWeek',  # week in season, should make a good/bad record later in season more important
-            'absLine',  # absolute value of spread since favored team already determined
-            'divisionGame', # T/F, usually more competitive rivalry games, i.e. bad teams still win home division games.
-            'favoredHomeGame', # T/F, important since output of classifier is "did the favored team win?"
-            ]
-
 # run the classifer
 random_state = 11
 svm_classifier = svm.SVC(kernel='poly', probability=True, random_state=random_state)
 lr_classifier = linear_model.LogisticRegression(C=1e5)
 
-svm_trained_classifier = madden.runScikitClassifier(dfAllGames, madden.FEATURE_COLUMNS, svm_classifier)
-lr_trained_classifier = madden.runScikitClassifier(dfAllGames, madden.FEATURE_COLUMNS, lr_classifier)
-
-# predict one week of current season
-week_number = WEEK_TO_PICK
+svm_trained_classifier = madden.runScikitClassifier(df_all_historical_games, madden.FEATURE_COLUMNS, svm_classifier)
+lr_trained_classifier = madden.runScikitClassifier(df_all_historical_games, madden.FEATURE_COLUMNS, lr_classifier)
 
 # should be only one year
 season_test = np.array(testYear)
@@ -109,9 +103,9 @@ dfTest = dfGamesTest[dfGamesTest.gameWeek == week_number]
 
 ###################################################################################################################
 # apply results of logistic regression to the test set
-df_svm_predict = madden.predictGames(dfTest, svm_trained_classifier, features)
+df_svm_predict = madden.predictGames(dfTest, svm_trained_classifier, madden.FEATURE_COLUMNS)
 # apply ranking logic and determine scoring outcomes for league
-dfAll = madden.rankGames(df_svm_predict, reference_data, season_test[0])
+df_all_picks = madden.rankGames(df_svm_predict, reference_data, season_test[0])
 
 # display weekly ranking output
 
@@ -121,40 +115,43 @@ dfAll = madden.rankGames(df_svm_predict, reference_data, season_test[0])
 # 2. pick winner based on abs(probability - .5), rank by probability
 # 3. pick winner based on abs(probability - .5), rank by abs(probability - .5)
 
-dispCols = ['season','gameWeek','Visitor','visitorRecord','Home Team','homeRecord',
+DISPLAY_COLUMNS = ['season','gameWeek','Visitor','visitorRecord','Home Team','homeRecord',
             'Line','prevFavoredRecord','prevUnderdogRecord','predict_proba',
             'lineGuess','probaGuess', 'probaAbsGuess', 'predictTeam']
 
 
-dfAll['predictTeam'] = np.where((dfAll['predict_proba'] - .5) > 0 , dfAll['favorite'], dfAll['underdog'])
+df_all_picks['predictTeam'] = np.where((df_all_picks['predict_proba'] - .5) > 0 , df_all_picks['favorite'], df_all_picks['underdog'])
 guessCol = 'probaGuess'
 predictCols = ['gameWeek','predictTeam', 'predict_proba', guessCol, 'favorite','lineGuess', 'Line']
 
-logging.info("Picks for week {0} using SVM".format(week_number))
-print("\nPicks using SVM")
-print(dfAll[predictCols].sort(guessCol, ascending=False))
+print("\nPicks for week {0:0>2} using SVM\n".format(week_number))
+svm_picks_df = df_all_picks[predictCols].sort(guessCol, ascending=False).copy()
+print(svm_picks_df)
+svm_picks_df.to_csv("svm_picks_week_{0:0>2}.csv".format(week_number), index=False)
 
-week_filter = dfAll.gameWeek == week_number
-print("\nPicks using SVM")
-print(dfAll[week_filter][predictCols].sort(guessCol, ascending=False))
+#week_filter = df_all_picks.gameWeek == week_number
+#print("\nPicks using SVM")
+#print(df_all_picks[week_filter][predictCols].sort(guessCol, ascending=False))
 ###################################################################################################################
 
 ###################################################################################################################
 # apply results of logistic regression to the test set
-df_lr_predict = madden.predictGames(dfTest, lr_trained_classifier, features)
+df_lr_predict = madden.predictGames(dfTest, lr_trained_classifier, madden.FEATURE_COLUMNS)
 # apply ranking logic and determine scoring outcomes for league
-dfAll = madden.rankGames(df_lr_predict, reference_data, season_test[0])
+df_all_picks = madden.rankGames(df_lr_predict, reference_data, season_test[0])
 
 # Use Method 2
-dfAll['predictTeam'] = np.where((dfAll['predict_proba'] - .5) > 0 , dfAll['favorite'], dfAll['underdog'])
+df_all_picks['predictTeam'] = np.where((df_all_picks['predict_proba'] - .5) > 0 , df_all_picks['favorite'], df_all_picks['underdog'])
 
-logging.info("Picks for week {0} using LogReg".format(week_number))
-print("\nPicks using LogReg")
-print(dfAll[predictCols].sort(guessCol, ascending=False))
+print("\nPicks for week {0:0>2} using LogReg\n".format(week_number))
+log_reg_picks_df = df_all_picks[predictCols].sort(guessCol, ascending=False).copy()
+print(log_reg_picks_df)
 
-week_filter = dfAll.gameWeek == week_number
-print("\nPicks using LogReg")
-print(dfAll[week_filter][predictCols].sort(guessCol, ascending=False))
+log_reg_picks_df.to_csv("log_reg_picks_week_{0:0>2}.csv".format(week_number), index=False)
+
+#week_filter = df_all_picks.gameWeek == week_number
+#print("\nPicks using LogReg\n")
+#print(df_all_picks[week_filter][predictCols].sort(guessCol, ascending=False))
 ###################################################################################################################
 
 # display weekly ranking output for spread method
@@ -168,7 +165,10 @@ print(dfAll[week_filter][predictCols].sort(guessCol, ascending=False))
 predictCols = ['favorite','lineGuess', 'absLine', 'Line', 'favoredHomeGame', 'divisionGame', 'favoredRecord']
 
 sortCols = ['absLine','favoredHomeGame', 'divisionGame', 'favoredRecord', 'favorite']
-dfSpread = dfAll[predictCols].sort(sortCols , ascending=False)
-#print(dfSpread.to_csv(sys.stdout, sep=',', index=False))
-print("\nHere are the Spread picks:")
-print(dfSpread)
+df_spread = df_all_picks[predictCols].sort(sortCols , ascending=False)
+
+print("\nPicks for week {0:0>2} using Spread\n".format(week_number))
+#print(df_spread.to_csv(sys.stdout, index=False))
+print(df_spread)
+
+df_spread.to_csv("spread_picks_week_{0:0>2}.csv".format(week_number), index=False)
